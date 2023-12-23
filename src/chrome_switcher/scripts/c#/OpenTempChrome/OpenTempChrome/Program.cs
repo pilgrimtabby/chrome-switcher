5a920d8da682915093ef7b14582e690e09f23a85
pilgrim_tabby <pilgrimtabby@gmail.com>
2023-12-23 07:27:11 -0700

    Add files via upload
    C# script that opens Chrome on windows with a temporary user-data-dir and waits for it to close before exiting

---
+++ src/chrome_switcher/scripts/c#/OpenTempChrome/OpenTempChrome/Program.cs
@@ -0,0 +1,30 @@
+﻿/**
+ * Starts a unique session of Google Chrome and waits until the session closes to exit.
+ * I call this from a Python script and wait on it. When it closes, the user data directory
+ * is deleted by the script.
+ * args[0]: Path to chrome.exe.
+ * args[1]: Desired user data directory path.
+ */
+using System;
+using System.Diagnostics;
+
+namespace OpenTempChrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #if DEBUG
+                args = new[] { "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe", 
+                               "C:\\Users\\test" };
+            # endif
+            var proc = new Process();
+            proc.StartInfo.FileName = args[0];
+            proc.StartInfo.Arguments = $"chrome://newtab --user-data-dir=\"{args[1]}\"";
+            proc.Start();
+            proc.WaitForExit();
+            Console.WriteLine(proc.ExitCode.ToString());
+            proc.Close();
+        }
+    }
+}
